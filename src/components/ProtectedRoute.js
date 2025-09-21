2fe7f4239cb2758ed4f664ca75c619cb6bd70128
azro1 <azro1.development@gmail.com>
2025-09-21 21:52:59 +0100

    Adjusted min height for mobiles

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -16,7 +16,7 @@ export default function ProtectedRoute({ children }) {
 
   if (loading) {
     return (
-      <div className="flex items-center justify-center min-h-[400px] sm:h-[calc(100vh-250px)]">
+      <div className="flex items-center justify-center min-h-[350px] sm:h-[calc(100vh-250px)]">
         <div className="text-center">
           <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-600 mx-auto"></div>
           <p className="mt-4 text-gray-600">Loading...</p>
