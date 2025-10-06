45a67b9e8a7b4b4d8f331675c87ffa37b5a7be89
azro1 <azro1.development@gmail.com>
2025-10-06 14:43:31 +0100

    Configure next.config remote patterns to allow loading of Google images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // App Router is now stable in Next.js 13+, no experimental flag needed
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
